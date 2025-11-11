19f9a166486f9a252c85e063fb8241109d8a0658
Van DUc Tan <vanductan.nlt@gmail.com>
2025-11-11 22:22:41 +0700

    feat: Dev Entry & Mode Switch

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import {Composition, registerRoot} from "remotion";
+import {Composition, getInputProps, registerRoot} from "remotion";
 import {Orchestrator} from "./core/Orchestrator";
 import {loadPlan} from "./core/loadPlan";
 import {getFps} from "./core/utils/fpsControl";
@@ -6,37 +6,45 @@ import {PreviewApp} from "./preview";
 import {AnimationPreviewApp} from "./library/animations/AnimationPreviewApp";
 import {totalFrames} from "./core/utils/frameUtils";
 
+const inputProps = getInputProps<{devMode?: boolean}>();
 const bootstrapPlan = loadPlan();
 const initialFps = getFps(bootstrapPlan.templateId);
 const plan = bootstrapPlan.fps === initialFps ? bootstrapPlan : loadPlan({fps: initialFps});
+const isDevMode = Boolean(inputProps.devMode);
 
 export const RemotionRoot: React.FC = () => (
   <>
-    <Composition
-      id="live-preview"
-      component={PreviewApp}
-      durationInFrames={totalFrames(plan.segments, initialFps)}
-      fps={initialFps}
-      width={1080}
-      height={1920}
-    />
-    <Composition
-      id="animation-browser"
-      component={AnimationPreviewApp}
-      durationInFrames={120}
-      fps={initialFps}
-      width={1080}
-      height={1920}
-    />
-    <Composition
-      id="auto-video"
-      component={Orchestrator}
-      durationInFrames={plan.durationInFrames}
-      fps={initialFps}
-      width={1080}
-      height={1920}
-      defaultProps={{plan, fps: initialFps}}
-    />
+    {isDevMode ? (
+      <>
+        <Composition
+          id="live-preview"
+          component={PreviewApp}
+          durationInFrames={totalFrames(plan.segments, initialFps)}
+          fps={initialFps}
+          width={1080}
+          height={1920}
+        />
+        <Composition
+          id="animation-browser"
+          component={AnimationPreviewApp}
+          durationInFrames={120}
+          fps={initialFps}
+          width={1080}
+          height={1920}
+        />
+      </>
+    ) : null}
+    {!isDevMode ? (
+      <Composition
+        id="auto-video"
+        component={Orchestrator}
+        durationInFrames={plan.durationInFrames}
+        fps={initialFps}
+        width={1080}
+        height={1920}
+        defaultProps={{plan, fps: initialFps}}
+      />
+    ) : null}
   </>
 );
 

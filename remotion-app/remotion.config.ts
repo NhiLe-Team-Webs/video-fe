42a3ccdda8194211d40b2353911ad7d11180abc0
Van DUc Tan <vanductan.nlt@gmail.com>
2025-11-11 18:55:29 +0700

    feat: Feature 1 – Project bootstrap & cấu trúc hệ thống

--- remotion-app/remotion.config.ts
+++
@@ -1,9 +0,0 @@
-import {Config} from "@remotion/cli/config";
-
-Config.setVideoImageFormat("jpeg");
-Config.setCodec("h264");
-Config.setOverwriteOutput(true);
-Config.setAudioCodec("aac");
-Config.setAudioBitrate("128k");
-Config.setPublicDir("./public");
-Config.setMaxTimelineTracks(500);
--- remotion-app/src/index.ts
+++
@@ -1,6 +0,0 @@
-import '@fontsource/montserrat/800.css';
-import '@fontsource/montserrat/900.css';
-import {registerRoot} from 'remotion';
-import {RemotionRoot} from './Root';
-
-registerRoot(RemotionRoot);

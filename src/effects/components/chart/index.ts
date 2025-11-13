c40cd725384a3beadec4ec0c58342213e1f36a0e
vanductan-NLT <vanductan.nlt@gmail.com>
2025-11-13 23:49:07 +0700

    Data Visualization Reveal (Animated Chart / Stats Graph)

--- src/effects/components/chart/index.ts
+++ src/effects/components/chart/index.ts
@@ -1,11 +1,13 @@
 import {EffectComponentMap} from "../../../types/EffectTypes";
 import {StatCard} from "./StatCard";
 import {TimelineReveal} from "./TimelineReveal";
+import {DataVisualizationReveal} from "./DataVisualizationReveal";
 
-export {StatCard, TimelineReveal};
+export {StatCard, TimelineReveal, DataVisualizationReveal};
 
 export const chartEffectComponents: EffectComponentMap = {
   "chart.statCard": StatCard,
   "chart.timelineReveal": TimelineReveal,
+  "chart.dataReveal": DataVisualizationReveal,
 };
 
